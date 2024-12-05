bcfcf2128daba125cd560e3a383b718398c6274c
rashednsu172@gmail.com <Rashedul islam>
2024-12-05 19:44:04 +0600

    all problemm solved

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 import Problem1 from "./Problems/problem1";
 import Problem2 from "./Problems/Problem2";
 import Problem3 from "./Problems/Problem3";
+import Problem5 from "./Problems/Problem5";
+import Problem9 from "./Problems/Problem9";
 
 function App() {
   return (
     <>
+      <h1>Assignment-9</h1>
       <Problem1 />
       <Problem2 />
       <Problem3 />
+      <Problem5 />
+      <Problem9 />
     </>
   );
 }
---
+++ src/Problems/Problem9.jsx
@@ -0,0 +1,13 @@
+const Problem9 = () => {
+  const MaxNumber = (numbers) => {
+    return Math.max(...numbers);
+  };
+
+  const numbers = [2, 10, 15, 50, 60];
+
+  console.log(MaxNumber(numbers));
+
+  return <div></div>;
+};
+
+export default Problem9;
